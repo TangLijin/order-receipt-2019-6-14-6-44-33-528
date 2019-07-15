33020277f2008dda830ab09dd72673c091664e02
LOUIS TANG (ITA-ISDC-ISD-OOCLL/ZHA) <TANGLO2@OOCL.COM>
2019-07-15 19:33:21 +0800

    refactor:shouldPrintCustomerInformationOnOrder

--- src/test/java/org/katas/refactoring/OrderReceiptTest.java
+++ src/test/java/org/katas/refactoring/OrderReceiptTest.java
@@ -10,11 +10,14 @@ import static org.assertj.core.api.Assertions.assertThat;
 public class OrderReceiptTest {
     @Test
     public void shouldPrintCustomerInformationOnOrder() {
+//        given
         Order order = new Order("Mr X", "Chicago, 60601", new ArrayList<LineItem>());
         OrderReceipt receipt = new OrderReceipt(order);
 
+//        when
         String output = receipt.printReceipt();
 
+//        then
         assertThat(output).contains("Mr X", "Chicago, 60601");
     }
 
